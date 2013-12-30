9e5163fd832e7697cfded770e7db05ec87ad0601
Berton Cobb <berton.cobb@e-hps.com>
2013-12-30 10:09:55 -0500

    Removing phpstorm comment

--- app/code/community/Hps/Securesubmit/Model/Resource/Storedcard/Collection.php
+++ app/code/community/Hps/Securesubmit/Model/Resource/Storedcard/Collection.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: berton
- * Date: 12/27/13
- * Time: 12:40 PM
- */
+
 class Hps_Securesubmit_Model_Resource_Storedcard_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract {
     protected function _construct()
     {
--- app/code/community/Hps/Securesubmit/Model/Storedcard.php
+++ app/code/community/Hps/Securesubmit/Model/Storedcard.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: berton
- * Date: 12/23/13
- * Time: 4:27 PM
- */
 
 class Hps_Securesubmit_Model_Storedcard  extends Mage_Core_Model_Abstract{
     protected function _construct()
