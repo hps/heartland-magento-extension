344d1e1c29bba39ab85b6734199d7527b202941c
Sergey Kalenyuk <sergey.kalenyuk@gmail.com>
2014-01-25 00:32:07 +0200

    Correct casing.

--- app/code/community/Hps/Securesubmit/Block/Form.php
+++ app/code/community/Hps/Securesubmit/Block/Form.php
@@ -1,5 +1,5 @@
 <?php
-class Hps_SecureSubmit_Block_Form extends Mage_Payment_Block_Form_Ccsave
+class Hps_Securesubmit_Block_Form extends Mage_Payment_Block_Form_Ccsave
 {
     protected function _construct()
     {
